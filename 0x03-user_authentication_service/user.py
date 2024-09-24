b9fa606b9e9d171cfa28db722594340301e777ca
Anas Asimi <anas2020cr7@gmail.com>
2024-09-24 22:20:53 +0100

    fix task 0. User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -7,9 +7,15 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
+    """
+    class User
+    Args:
+        Base (_type_): _description_
+    """
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
