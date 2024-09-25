632d784ee10ac7873ac52d140043e39eae2ad4d8
Anas Asimi <anas2020cr7@gmail.com>
2024-09-25 11:46:03 +0100

    task 3. update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -54,3 +54,14 @@ class DB:
             raise NoResultFound()
         except InvalidRequestError:
             raise InvalidRequestError()
+
+    def update_user(self,user_id, **kwargs) -> User:
+        """Update a user from the database.
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                setattr(user, key, value)
+            self._session.commit()
+        except ValueError:
+            raise ValueError()
