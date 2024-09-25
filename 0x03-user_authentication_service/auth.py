a4d93950e794d46fbafbfefaaef46824aa02f7a5
Anas Asimi <anas2020cr7@gmail.com>
2024-09-25 13:11:03 +0100

    task 4. Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+auth.py
+"""
+
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """
+    _hash_password
+    Args:
+        password (str):
+    Returns:
+        bytes:
+    """
+    # converting password to array of bytes
+    bytes = password.encode('utf-8')
+    # generating the salt
+    salt = bcrypt.gensalt()
+    # Hashing the password
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
